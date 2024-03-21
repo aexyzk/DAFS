7727d38d28ac7b7a26f739832a8af647c3661fb6
ae <126417832+aexyzk@users.noreply.github.com>
2024-03-21 14:32:38 -0500

    a

--- src/main.py
+++ src/main.py
@@ -4,9 +4,6 @@ import sort_music as sort
 import get_links
 import get_linux_or_windows
 
-global line_number
-line_number = 0
-
 def main():
     print("**********************************************************************")
     print("*                      Spotify Album Downloader                      *")
@@ -18,25 +15,34 @@ def main():
 
     choice = 0
 
-    while choice != 3:
-        print("(1) Download Saved Songs!\n(2) Search Songs to Save!\n(3) Exit")
+    while choice != 4:
+        print("(1) Download Saved Songs!\n(2) Search Songs to Save!\n(3) Download Saved Videos!\n(4) Exit")
         choice = int(input("Enter choice: "))
 
         if choice == 1:
             with open("albums.txt", "r") as albums:
                 for i in albums:
-                    global line_number
-                    line_number += 1
-                    print(f"Downloading {line_number}: {i}")
+                    print(f"Downloading: {i}")
                     if get_linux_or_windows.isLinux() == False:
                         subprocess.run(f"spotdl.exe --output Music {i}")
                     elif get_linux_or_windows.isLinux() == True:
                         subprocess.run(["./spotdl-4.2.4-linux", "--output", "Music", i])
                     sort.sort()
+                line_number = 0
             choice = 0
         elif choice == 2:
             get_links.save_songs()
             choice = 0
+        elif choice == 3:
+            with open("videos.txt", "r") as videos:
+                for i in videos:
+                    print(f"Downloading: {i}")
+                    if get_linux_or_windows.isLinux() == False:
+                        subprocess.run(f"echo {i}")
+                    elif get_linux_or_windows.isLinux() == True:
+                        subprocess.run(["./yt-dlp_linux", i])
+                    sort.sort()
+                video_line_number = 0
 
 if __name__ == "__main__":
     main()
